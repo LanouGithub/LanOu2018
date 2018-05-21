4865380538bfd762156e7b82799a363cdb446e23
LANOU-YF-\Administrator <lokj2018@sina.com>
2018-05-21 19:53:26 +0800

    上传测试

---
+++ Assets/Sprites/Const/ConstDate.cs
@@ -0,0 +1,21 @@
+﻿/// <summary>
+/// 存储所有常量路径
+/// </summary>
+public class ConstDate
+{
+    #region 所有UI预制体文件夹
+    public const string UIPrefabsPath = "Prefabs/UIPrefabs";
+    #endregion
+
+    #region 音乐
+    #endregion
+
+    #region 音效
+    #endregion
+
+    #region 主要常用名称
+    #endregion
+
+    #region 固定位置
+    #endregion
+}
